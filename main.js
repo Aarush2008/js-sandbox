99f6029e7a0e4d01a3dd9b2e98502eed9501976a
Aarush2008 <98787426+Aarush2008@users.noreply.github.com>
2024-01-15 09:14:36 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Program Title
+
+// BINARY IF STATEMENTS
+// Boolean Data Type - true of false
+
+let isFriday = true;
+if (isFriday) {
+  console.log("Must be Pizza for dinner.");
+} else {
+  console.log("Probably no pizza tonight!");
+}
